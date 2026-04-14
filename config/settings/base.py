420ddf78f2c82c60ec1446c606abcb75a7c8a89a
crazyvenky <venkatesh5082931@gmail.com>
2026-04-15 05:18:12 +0530

    Add ProxyIPMiddleware to force Django to read Nginx IP headers

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'apps.accounts.middleware.ProxyIPMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
