561fb2b46e5b3bb8b978a5248baeb84a5f0bc872
crazyvenky <venkatesh5082931@gmail.com>
2026-04-21 17:23:08 +0530

    optimized search functionality

--- apps/search/views.py
+++ apps/search/views.py
@@ -5,49 +5,54 @@ from apps.titles.models import Title
 from .utils import fetch_tmdb_titles, sync_tmdb_to_local
 
 def search_results_view(request):
+
     query = request.GET.get('q', '').strip()
     search_type = request.GET.get('s', 'all')
     
     results = Title.objects.none()
 
     if query:
-        def get_local_results():
-            """Bulletproof local query that avoids NULL SearchVector crashes."""
+        def get_local_results(current_query):
             qs = Title.objects.filter(is_deleted=False)
             
             if search_type in ['all', 'titles']:
-                # Use Trigram for typos + standard icontains for absolute matches
-                return qs.annotate(
-                    similarity=TrigramSimilarity('title', query)
-                ).filter(
-                    Q(similarity__gt=0.1) | 
-                    Q(title__icontains=query) |
-                    Q(description__icontains=query)
-                ).order_by('-popularity', '-similarity')
+                qs = qs.annotate(similarity=TrigramSimilarity('title', current_query))
+                title_match = Q(similarity__gt=0.3) | Q(title__icontains=current_query)
+                
+                desc_match = Q(id__isnull=True)
+                if len(current_query) > 4 and not current_query.isdigit():
+                    desc_match = Q(description__icontains=current_query)
+
+                return qs.filter(title_match | desc_match).order_by(
+                    '-similarity',
+                    '-popularity'
+                )
                 
             elif search_type == 'directors':
-                return qs.filter(cast_members__person__name__icontains=query, cast_members__role='DIR').order_by('-popularity')
+                return qs.filter(
+                    cast_members__person__name__icontains=current_query, 
+                    cast_members__role='DIR'
+                ).order_by('-popularity')
                 
-            elif search_type == 'year' and query.isdigit():
-                return qs.filter(release_date__year=query).order_by('-popularity')
+            elif search_type == 'year' and current_query.isdigit():
+                return qs.filter(release_date__year=current_query).order_by('-popularity')
                 
             return qs
 
-        # 1. Check local Postgres DB
-        local_results = get_local_results()
-
-        # 2. Waterfall Logic: Ask TMDB for help if we have less than 3
-        if local_results.count() < 3 and search_type in ['all', 'titles']:
+        # Step 1: Initial Local Check
+        local_results = get_local_results(query)
+        results_list = list(local_results[:20]) 
+        if len(results_list) < 3 and search_type in ['all', 'titles']:
             tmdb_data = fetch_tmdb_titles(query)
-            
+            if not tmdb_data and len(query.split()) > 1:
+                relaxed_query = " ".join(query.split()[:-1])
+                tmdb_data = fetch_tmdb_titles(relaxed_query)
+
             if tmdb_data:
                 sync_tmdb_to_local(tmdb_data)
-                
-                # Re-query the database to pull the 20 newly saved movies!
-                local_results = get_local_results()
+                results_list = list(get_local_results(query)[:20])
 
-        # 3. Lock in top 20
-        results = local_results[:20]
+        results = results_list
 
     context = {
         'query': query,
